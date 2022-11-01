6a6b610aad43741eda1d1e82d480e998c241a937
Rukmangadh Sai Myana <rukman.sai@gmail.com>
2022-11-01 10:44:28 -0400

    Add documentation for the pcbmanager class

--- nachos-3.4-linux/nachos-3.4/code/userprog/pcbmanager.cc
+++ nachos-3.4-linux/nachos-3.4/code/userprog/pcbmanager.cc
@@ -1,30 +1,45 @@
 #include "pcbmanager.h"
 
+//---------------------------------------------------------------------
+// PCBManager::PCBManager
+//  Create a pcb manager that can manage the given number of processes
+//
+//  "maxProcesses" is the number of processes that it can manage
+//---------------------------------------------------------------------
 
-PCBManager::PCBManager(int maxProcesses) {
-
+PCBManager::PCBManager(int maxProcesses)
+{
     bitmap = new BitMap(maxProcesses);
-    pcbs = new PCB*[maxProcesses];
+    pcbs = new PCB *[maxProcesses];
     printf("pcbs.size() = %d\n", sizeof(pcbs));
 
-    for(int i = 0; i < maxProcesses; i++) {
+    for (int i = 0; i < maxProcesses; i++)
+    {
         pcbs[i] = NULL;
     }
-
 }
 
+//--------------------------------------------------------------------
+// PCBManager::~PCBManager
+//  Destructor.
+//--------------------------------------------------------------------
 
-PCBManager::~PCBManager() {
-
+PCBManager::~PCBManager()
+{
     delete bitmap;
 
     delete pcbs;
-
 }
 
+//--------------------------------------------------------------------
+// PCBManager::AllocatePCB
+//  Allocate a pcb to the calling process
+//
+//  Returns a pointer to the allocated pcb
+//--------------------------------------------------------------------
 
-PCB* PCBManager::AllocatePCB() {
-
+PCB *PCBManager::AllocatePCB()
+{
     // Aquire pcbManagerLock
 
     int pid = bitmap->Find();
@@ -36,26 +51,41 @@ PCB* PCBManager::AllocatePCB() {
     pcbs[pid] = new PCB(pid);
 
     return pcbs[pid];
-
 }
 
+//--------------------------------------------------------------------
+// PCBManager::DeallocatePCB
+//  Deallocate (delete) the pcb instance and clean up after it
+//
+//  "pcb" is the pcb you want to deallocate
+//
+//  Returns 0 if successful otherwise -1
+//--------------------------------------------------------------------
 
-int PCBManager::DeallocatePCB(PCB* pcb) {
-
+int PCBManager::DeallocatePCB(PCB *pcb)
+{
     // Check is pcb is valid -- check pcbs for pcb->pid
 
-     // Aquire pcbManagerLock
+    // Aquire pcbManagerLock
 
-    bitmap->Clear(pcb->pid);
+    int process_id = pcb->getPID();
+    bitmap->Clear(process_id);
 
     // Release pcbManagerLock
 
-    delete pcbs[pcb->pid];
+    delete pcbs[process_id];
 
-    pcbs[pcb->pid] = NULL;
-
+    pcbs[process_id] = NULL;
 }
 
-PCB* PCBManager::GetPCB(int pid) {
+//--------------------------------------------------------------------
+// PCBManager::GetPCB
+//  Return a pointer to the pcb instance for the given pid
+//
+//  "pid" is the given pid
+//--------------------------------------------------------------------
+
+PCB *PCBManager::GetPCB(int pid)
+{
     return pcbs[pid];
 }
