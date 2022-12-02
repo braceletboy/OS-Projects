70325921a0c8b60442512fdf6952ead08463a1ac
Rukmangadh Sai Myana <rukman.sai@gmail.com>
2022-12-02 13:58:14 -0500

    Add reference count to OFDs

--- nachos-3.4-linux/nachos-3.4/code/userprog/ofd.cc
+++ nachos-3.4-linux/nachos-3.4/code/userprog/ofd.cc
@@ -20,6 +20,7 @@ OFD::OFD(char *fileName, int id)
     ofdID = id;
     name = fileName;
     fileVNode = vnm->AssignVNode(fileName);
+    refCount = 1;  // one FD points to this OFD at creation
 }
 
 //------------------------------------------------------------------------
@@ -40,4 +41,34 @@ OFD::~OFD()
 int OFD::GetID()
 {
     return ofdID;
+}
+
+//------------------------------------------------------------------------
+// OFD::IncreaseRef
+//  Return the index of the OFD in the Open File Table.
+//------------------------------------------------------------------------
+void OFD::IncreaseRef()
+{
+    refCount++;
+}
+
+//------------------------------------------------------------------------
+// OFD::DecreaseRef
+//  Return the index of the OFD in the Open File Table.
+//------------------------------------------------------------------------
+void OFD::DecreaseRef()
+{
+    ASSERT(refCount > 0);
+    refCount--;
+}
+
+//------------------------------------------------------------------------
+// OFD::IsActive
+//  Return whether the OFD is being used by processes or not.
+//
+//  If there are no FDs pointing to the OFD then it's not active.
+//------------------------------------------------------------------------
+bool OFD::IsActive()
+{
+    return (refCount > 0);
 }
--- nachos-3.4-linux/nachos-3.4/code/userprog/ofd.h
+++ nachos-3.4-linux/nachos-3.4/code/userprog/ofd.h
@@ -2,11 +2,19 @@
 //  This file contains the class for a User Open File Object mentioned in the
 //  assignment. This concept is analogous to the concept of an Open File
 //  Description (OFD) in Unix. An OFD is also called an open file table entry
-//  i.e. an entry in the Open File Table in unix.
+//  i.e. an entry in the Open File Table in unix. An OFD represents an open
+//  connection between a process and a file.
 //
 //  In Unix, every open() system call will have its own OFD. Similarly, in our
 //  Nachos implementation, we shall have one OFD for every Open system call.
-//  Multiple OFD instances can point to the same VNode instance.
+//  Multiple OFD instances can point to the same VNode instance. Such a
+//  scenario arises when multiple open system calls are made to the same file
+//  either by different processes or the same process i.e. there are multiple
+//  connections to the file from different or same process respectively.
+//
+//  Similarly, there can be multiple file descriptors (FDs) pointing to the
+//  same OFD. This happens when a process forks and the child process inherits
+//  the same file descriptors.
 //
 
 #include "vnode.h"
@@ -18,9 +26,13 @@ class OFD
         ~OFD();
 
         int GetID();
+        void IncreaseRef();
+        void DecreaseRef();
+        bool IsActive();
 
     private:
         int ofdID;  // index of the OFD in the Open File Table
         char *name;  // name of the file
         VNode *fileVNode;  // Associated VNode
+        int refCount;  // the number of file descriptors pointing to the OFD
 };
