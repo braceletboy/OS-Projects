8e1adbee5e454b1a2d27aa84fe4ef72d7eae8b36
Rukmangadh Sai Myana <rukman.sai@gmail.com>
2022-12-02 12:07:00 -0500

    Add a preliminary implementation of the system wide open file table

---
+++ nachos-3.4-linux/nachos-3.4/code/userprog/vnodemanager.cc
@@ -0,0 +1,98 @@
+#include "vnodemanager.h"
+#include "system.h"
+
+//------------------------------------------------------------------------
+// VNodeManager::VNodeManager
+//  Constructor.
+//------------------------------------------------------------------------
+VNodeManager::VNodeManager()
+{
+	vnodes = new List();
+	vnmLock = new Lock("vnode manager lock");
+}
+
+//------------------------------------------------------------------------
+// VNodeManager::~VNodeManager
+//  Destructor.
+//------------------------------------------------------------------------
+VNodeManager::~VNodeManager()
+{
+	delete vnodes;
+    delete vnmLock;
+}
+
+//------------------------------------------------------------------------
+// VNodeManager::AssignVNode
+//  Associate VNode with the given file name to the current process.
+//
+//  "fileName" the file name corresponding to the VNode.
+//
+//  Returns a pointer the associated VNode.
+//------------------------------------------------------------------------
+VNode *VNodeManager::AssignVNode(char *fileName)
+{
+	vnmLock->Acquire();
+
+    VNode *allocated_vnode;
+
+    // if vnode already exists - return the already allocated vnode
+    // iterate through the items in the vnode list by calling remove
+    List *tempList = new List();
+    VNode *removed_vnode = (VNode *) vnodes->Remove();
+    while(removed_vnode != NULL)
+    {
+        //removed vnode is the allocated vnode
+        if(strcmp(fileName, removed_vnode->GetFileName()) == 0)
+        {
+            removed_vnode->IncreaseRef();  // another proc wants to access
+            allocated_vnode = removed_vnode;
+
+            // put back the removed vnodes in the correct order
+            vnodes->Prepend((void *) removed_vnode);
+            while(!tempList->IsEmpty())
+            {
+                void *item = tempList->Remove();
+                vnodes->Prepend(item);
+            }
+
+            return allocated_vnode;
+        }
+
+        // store removed item in the temp list
+        tempList->Prepend((void *) removed_vnode);
+
+        removed_vnode = (VNode *) vnodes->Remove();
+    }
+
+    // vnode doesn't already exist - reset the vnodes list
+    delete vnodes;
+    vnodes = tempList;
+
+    // allocate new vnode and store it
+    allocated_vnode = new VNode(fileName);
+    vnodes->Append((void *) allocated_vnode);
+
+    vnmLock->Release();
+
+    return allocated_vnode;
+}
+
+//------------------------------------------------------------------------
+// VNodeManager::RelieveVNode
+//  Dis-associate VNode from the current process.
+//
+//  Does nothing if the vnode provided is not associated with the current
+//  process.
+//
+//  "vnode" is the VNode of the current process.
+//------------------------------------------------------------------------
+void VNodeManager::RelieveVNode(VNode* vnode)
+{
+    vnmLock->Acquire();
+
+    if(currentThread) // TODO
+    vnode->DecreaseRef();
+    if(!vnode->IsActive()) vnodes->RemoveItem((void *) vnode);
+
+    vnmLock->Release();
+}
