7c1ea6dacc8ec0fa544c6de041e9010a58172873
Rukmangadh Sai Myana <rukman.sai@gmail.com>
2022-12-04 04:39:44 -0500

    Fix bug in synch

--- nachos-3.4-linux/nachos-3.4/code/threads/synch.h
+++ nachos-3.4-linux/nachos-3.4/code/threads/synch.h
@@ -19,6 +19,9 @@
 
 #include "copyright.h"
 #include "list.h"
+#ifndef USER_PROGRAM
+#include "thread.h"
+#endif
 
 // The following class defines a "semaphore" whose value is a non-negative
 // integer.  The semaphore has only two operations P() and V():
